00b00ccd4115b1409bd25ab0b33dd72ac15e7f5d
viveksg <viveksgt@gmail.com>
2018-05-15 00:51:50 +0530

    added tags to text buffer for breakpoint highlighting

--- ui/main_window.py
+++ ui/main_window.py
@@ -9,6 +9,10 @@ class MainWindow(Gtk.ApplicationWindow):
     stack = None
     memory = None 
     current_file_loc = None
+    tags = dict()
+    tags_selection = dict()
+    colors = ["orange","white"]
+    file_opened = False
     def __init__(self, app):
         Gtk.Window.__init__(self,title = "GGDB_X86",application = app)
         self.set_border_width(10)
@@ -58,14 +62,23 @@ class MainWindow(Gtk.ApplicationWindow):
         self.current_file_loc = dialog.get_filename()
         file = open(self.current_file_loc,"r")
         self.code.setText(file)
+        self.file_opened = True
+        self.initTags()
         file.close()
-        
+    
+    def initTags(self):
+        buffer = self.code.getTextBuffer()
+        total_line = buffer.get_line_count()
+        for i in range(total_line+1):
+           self.tags[i] = None
+           self.tags_selection[i] = -1    
     
     def addScrolledWindows(self):
        grid = Gtk.Grid()
        box_primary = Gtk.Box()
        box_secondary = Gtk.Box(orientation = Gtk.Orientation.VERTICAL, spacing = 6)
        self.code = ScrollWindow()
+       self.code.registerCallback(self.codeClickedHandler)
        self.registers = ScrollWindow()
        self.memory = ScrollWindow()
        self.stack = ScrollWindow()
@@ -80,8 +93,28 @@ class MainWindow(Gtk.ApplicationWindow):
        box_secondary.add(self.memory)
        box_secondary.add(Gtk.Separator(orientation = Gtk.Orientation.HORIZONTAL))
        box_secondary.add(self.stack)
-       self.add(grid)       
-
+       self.add(grid)    
+  
+    def codeClickedHandler(self,widget,data):
+       if not self.file_opened:
+           return
+       buffer = self.code.getTextBuffer()
+       current_line = self.getCurrentLine(buffer)
+       self.addTag(buffer, current_line)
+   
+    def getCurrentLine(self,buffer):
+       buffer_props = buffer.props
+       current_iter = buffer.get_iter_at_offset(buffer_props.cursor_position)
+       current_line = current_iter.get_line()
+       return current_line
+   
+    def addTag(self,buffer,current_line):
+       self.tags_selection[current_line] = (self.tags_selection[current_line] + 1) % 2
+       if self.tags[current_line] is None:
+           self.tags[current_line]  = buffer.create_tag("tag_"+str(current_line),background = "orange")
+           buffer.apply_tag(self.tags[current_line],buffer.get_iter_at_line(current_line),buffer.get_iter_at_line(current_line + 1)) 
+           return
+       self.tags[current_line].props.background = self.colors[self.tags_selection[current_line]]
 
 class ImageButton(Gtk.Button):
     def __init__(self,icon_name):
@@ -103,6 +136,14 @@ class ScrollWindow(Gtk.ScrolledWindow):
          buffer = self.text_view.get_buffer()
          buffer.set_text(file.read())
     
+    def registerCallback(self,handler):
+         window = self.text_view.get_window(Gtk.TextWindowType.TEXT)
+         #self.add_events(Gdk.BUTTON_PRESS_MASK)
+         self.connect('button-press-event', handler)
+         #self.connect('clicked',handler)
+    def getTextBuffer(self):
+         buffer = self.text_view.get_buffer()
+         return buffer    
          
 class MainApp(Gtk.Application):
     def __init__(self):
